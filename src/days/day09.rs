adaca861e506f3db9e5d990f733474286dea4bb4
Gabriele Girelli <gigi.ga90@gmail.com>
2022-12-09 08:54:34 +0100

    feat: base for day 09

---
+++ src/days/day09.rs
@@ -0,0 +1,25 @@
+use crate::utils::io::read_rows;
+
+pub fn pt1(file_path: String) -> i32 {
+    let _rows: Vec<String> = read_rows(&file_path);
+    0
+}
+
+#[test]
+fn test_pt1() {}
+
+pub fn pt2(file_path: String) -> i32 {
+    let _rows: Vec<String> = read_rows(&file_path);
+    0
+}
+
+#[test]
+fn test_pt2() {}
+
+pub fn run(part: i32) {
+    let _result: i32 = match part {
+        1 => pt1("data/day08.txt".to_string()),
+        2 => pt2("data/day08.txt".to_string()),
+        _ => panic!("Part {} not found.", part),
+    };
+}
