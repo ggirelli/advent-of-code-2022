95ac6e0aaa6da03dffd1385715094b6d34aa9f77
Gabriele Girelli <gigi.ga90@gmail.com>
2022-12-18 20:35:22 +0100

    feat: added packet grouping

--- src/days/day13.rs
+++ src/days/day13.rs
@@ -1,7 +1,44 @@
-use crate::utils::io::read_rows;
+use crate::utils::io::{group_rows, read_rows};
+
+fn assert_packet_is_list(packet: &String) {
+    assert_eq!(packet.chars().nth(0).unwrap(), '[');
+    assert_eq!(packet.chars().nth(packet.len() - 1).unwrap(), ']');
+}
+
+#[test]
+fn test_assert_packet_is_list() {
+    assert_packet_is_list(&String::from("[1,2,3,4]"))
+}
+
+#[test]
+#[should_panic]
+fn test_assert_packet_is_list_fail() {
+    assert_packet_is_list(&String::from("[1,2,3,4"))
+}
+
+fn extract_next_value(packet: &String) -> (String, String) {
+    let mut position: usize = 0;
+    if packet.chars().nth(position).unwrap() == '[' {
+        assert!(packet[1..].contains(']'));
+    } else {
+    }
+    (String::from(""), String::from(""))
+}
+
+fn parse_and_compare_packets(p1: &String, p2: &String) -> bool {
+    assert_packet_is_list(p1);
+    assert_packet_is_list(p2);
+    true
+}
 
 pub fn pt1(file_path: String) -> i32 {
     let _rows: Vec<String> = read_rows(&file_path);
+    let packet_pairs: Vec<Vec<String>> = group_rows(&_rows, String::from(""));
+    for pair in packet_pairs {
+        if pair.len() != 2 {
+            panic!("Found {} packets instead of 2.", pair.len());
+        }
+    }
     0
 }
 
--- src/utils/io.rs
+++ src/utils/io.rs
@@ -8,3 +8,20 @@ pub fn read_rows(file_path: &String) -> Vec<String> {
         .map(String::from)
         .collect();
 }
+
+pub fn group_rows(_rows: &Vec<String>, sep: String) -> Vec<Vec<String>> {
+    let mut row_groups: Vec<Vec<String>> = Vec::new();
+    let mut current_group: Vec<String> = Vec::new();
+    for row in _rows {
+        if row == &sep {
+            row_groups.push(current_group);
+            current_group = Vec::new();
+        } else {
+            current_group.push(row.to_string());
+        }
+    }
+    if current_group.len() > 0 {
+        row_groups.push(current_group);
+    }
+    row_groups
+}
