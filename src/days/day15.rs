bb677669ba83689a85ad4d1743ba096054b0713a
Gabriele Girelli <gigi.ga90@gmail.com>
2023-02-05 14:47:12 +0100

    feat: solved day 15 pt 1

--- src/days/day15.rs
+++ src/days/day15.rs
@@ -1,13 +1,30 @@
+use crate::utils::exclusion_zone::{BeaconSensorDB, Map};
 use crate::utils::io::read_rows;
 
-pub fn pt1(file_path: String) -> i32 {
+fn count_not_beacon_positions(file_path: String, row_coord: i32, verbose: usize) -> usize {
     let _rows: Vec<String> = read_rows(&file_path);
-    0
+    let _bsdb: BeaconSensorDB = BeaconSensorDB::from_text(&_rows);
+    let map: Map = Map::from_beacon_sensors(&_bsdb);
+    if verbose > 0 {
+        _bsdb.print();
+        map.print_details();
+    }
+    if verbose > 1 {
+        map._print(&_bsdb);
+    }
+    map.count_not_beacon_positions(row_coord, &_bsdb)
+}
+
+pub fn pt1(file_path: String) -> i32 {
+    count_not_beacon_positions(file_path, 2000000, 0) as i32
 }
 
 #[test]
 fn test_pt1() {
-    assert_eq!(pt1("data/day15.test.txt".to_string()), 26);
+    assert_eq!(
+        count_not_beacon_positions("data/day15.test.txt".to_string(), 10, 2) as i32,
+        26
+    );
 }
 
 pub fn pt2(file_path: String) -> i32 {
